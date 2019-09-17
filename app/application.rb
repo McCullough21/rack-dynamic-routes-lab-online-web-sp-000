cad41aa7dca1ee834a7c68298bf864ab1362dd04
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-17 18:32:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ def call(env)
         resp.write item.price
       else
         resp.write "Item not found"
-        resp.status 400
+        resp.status = 400
       end
   else
     resp.write "Route not found"
