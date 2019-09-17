451f8258588f9d2df092885cf07360cf48c7fb05
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-17 18:08:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,9 @@ def call(env)
 
   if req.path.match (/items/)
     item_name = req.path.split("/items/").last
-      item = @@items.find{|s| s.name == item_name}
+      if @@items.include?(item_name)
       resp.write item.price
+      end
   end
 end
 
