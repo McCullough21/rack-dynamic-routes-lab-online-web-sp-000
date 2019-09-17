031be3ba0bde61367375526c85028185dc27c2ac
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-17 18:27:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,7 @@ def call(env)
   if req.path.match ("/items/")
     if
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+      item = @@items.find{|i| i.name == item_name resp.write item.price}
     else
       resp.write "Item not found"
       resp.status 400
