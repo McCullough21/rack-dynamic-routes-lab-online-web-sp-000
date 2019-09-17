ee2c6d7343edfb0d64e8e6ce7afb28fa83395395
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-17 18:10:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
-  if req.path.match (/items/)
+  if req.path=="/items/"
     item_name = req.path.split("/items/").last
       if @@items.include?(item_name)
       resp.write item.price
