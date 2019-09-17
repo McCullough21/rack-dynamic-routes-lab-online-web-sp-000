078c3055cddd25c6ec5f73487065b48999c38e96
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-17 18:21:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,11 @@ def call(env)
   if req.path.match ("/items/")
     item_name = req.path.split("/items/").last
     item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+      if !item
+        resp.write "Item not found"
+        resp.status 400
+      else
+        resp.write item.price
   else
     resp.write "Route not found"
       resp.status = 404
